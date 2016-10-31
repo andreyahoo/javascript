24a557ae8d5ae32976382097986f4222a1c30848
YuriDPaula <yurisamuka@hotmail.com>
2016-10-31 14:21:04 -0200

    exercicio em java

---
+++ tutorial/2-JS WHERE TO/funcoes.js
@@ -0,0 +1,13 @@
+window.onload =function  () {
+	// console.log(window);
+	document.getElementById("botaoMudar").addEventListener("click", function(){
+		document.getElementById("demo").innerHTML="<h1>Mudar paragrafo</h1>";
+	});
+}
+
+// function MinhaFuncao(){
+// 		// window.alert("resultado da funçoes");
+// 	document.getElementById("demo").innerHTML="<a>Mudar paragrafo";
+// }
+  
+
