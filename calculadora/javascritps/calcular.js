bfcda6a434e1b59197b825e6d3bc3766049591cf
YuriDPaula <yurisamuka@hotmail.com>
2016-11-04 14:07:33 -0200

    to aprendendo fazer uma calculadora foda

---
+++ calculadora/javascritps/calcular.js
@@ -0,0 +1,21 @@
+// andre.js
+var contval="";
+function exebirnaTela(valor){
+	contval =contval+valor;
+	document.getElementById('tela').value =contval;
+
+
+
+
+
+
+
+// alert(valor);
+// document.getElementById('tela').value =document.getElementById('tela').value+valor;
+// 
+}
+
+
+// "andre"
+// andre
+// andre()
