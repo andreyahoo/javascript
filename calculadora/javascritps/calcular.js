b348f0ef89659ed44a1085c73f7def758132fda6
YuriDPaula <yurisamuka@hotmail.com>
2016-11-09 15:13:05 -0200

    texte

--- calculadora/javascritps/calcular.js
+++ calculadora/javascritps/calcular.js
@@ -1,21 +1,57 @@
 // andre.js
-var contval="";
+var numTela="";
+var num1 ="";
+var num2 ="";
+var sinal;
 function exebirnaTela(valor){
-	contval =contval+valor;
-	document.getElementById('tela').value =contval;
-
-
-
-
-
-
-
+	numTela = numTela+valor;
+	document.getElementById('tela').value =numTela;
 // alert(valor);
 // document.getElementById('tela').value =document.getElementById('tela').value+valor;
 // 
 }
 
+function calcular (operador){
 
+
+
+	// alert (contval);
+
+if(operador != "="){
+	sinal = operador;
+}
+
+
+	if(num1==""){
+		num1=numTela;
+		numTela = "";
+		document.getElementById('tela').value ="";
+	}else{
+		num2=numTela;
+		numTela = "";
+		document.getElementById('tela').value ="";
+		resultado(operador);
+	}
+
+
+}
+
+
+function resultado(s){
+	var test;
+	// alert(test);
+	if(sinal=="+"){
+		test = parseInt(num1)+parseInt(num2);
+	}else if(sinal=="-"){
+		test = parseInt(num1)-parseInt(num2);
+	}else if(sinal=="*"){
+		test = parseInt(num1)*parseInt(num2);
+	}else if(sinal=="/"){
+		test = parseInt(num1)/parseInt(num2);
+	}
+	document.getElementById('tela').value =test;
+
+}
 // "andre"
 // andre
 // andre()
